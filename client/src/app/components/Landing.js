1dc202140969ceefcb9e36c3878816bdb5684bdb
isek27 <isaacsek@uw.edu>
2018-01-11 19:30:09 -0800

    header

--- client/src/app/components/Landing.js
+++ client/src/app/components/Landing.js
@@ -7,9 +7,11 @@ class Landing extends Component {
     render() {
         return (
             <div style={{textAlign:'center'}}>
-                <h1>
+                <div className="mt-2"><h2>CSS 490</h2></div>
+                <div><h2>Multimedia Data Processing</h2></div>
+                {/* <h1>
                     CSS 490: Multimedia Data Processing
-                </h1>
+                </h1> */}
                 {/* <h5 className="mb-2">Simple Content-Based Image Retrieval System</h5> */}
             </div>
         );
